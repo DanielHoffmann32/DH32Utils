5da371afc5efe6ae22e318d793f8c8470ed8d13c
Daniel Hoffmann <daniel.hoffmann@uni-due.de>
2017-12-15 14:54:19 +0100

    added test for block_average_spread

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,10 @@
 using DH32Utils
 using Base.Test
+using GZip
 
 # write your own tests here
-@test 1 == 1
+x = vec(readcsv(gzopen("testdist.csv.gz")))
+w = vec(readcsv(gzopen("testweights.csv.gz")))
+q1,q2 = block_average_spread(x, dimin=1000, ddi=1000, weighted=true, w=w)
+@test abs(q1[1]-1.7432) < 0.001
+@test abs(q2[1]-2.0877) < 0.001
